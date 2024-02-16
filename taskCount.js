0589fa225feba73cef065c1d3158d347f63c5188
sallyaz22 <115486590+sallyaz22@users.noreply.github.com>
2024-02-16 19:46:02 +0200

    first commit

---
+++ taskCount.js
@@ -0,0 +1,23 @@
+let count = 0;
+function increment(){
+    count = count + 1
+    document.querySelector(".startone").innerText = count;
+}
+let countOne = 0;
+function incrementone(){
+    countOne = countOne + 1
+    document.querySelector(".h1").innerText = countOne;
+}
+
+let countTwo = 0;
+function incrementTwo(){
+    countTwo = countTwo + 1
+    document.querySelector(".h2").innerText = countTwo;
+}
+
+let countThree = 0;
+function incrementThree(){
+    countThree = countThree + 1
+    document.querySelector(".h3").innerText = countThree;
+}
+
